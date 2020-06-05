ec40633a21cd18cf8a506067f554794f111a9c5f
Bartosz Rączka <barteksae@gmail.com>
2020-06-05 20:26:33 +0200

    [CC]

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by MacBook on 03/06/2020.
+//  Copyright © 2020 Bartosz Raczka Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func loadView() {
+        super.loadView()
+        view.backgroundColor = .cyan
+    }
+
+}
+
